777d9ea643ae125593630adfd6abd6e219474e7b
liuyufeng <liuyufeng@xywy.com>
2016-09-19 15:37:57 +0800

    手机摄像头测心率

--- HeartLive.h
+++ HeartLive.h
@@ -5,6 +5,18 @@
 //  Created by A053 on 16/9/9.
 //  Copyright © 2016年 Yvan. All rights reserved.
 //
+/*
+ 
+ //创建了一个心电图的View
+ self.live = [[HeartLive alloc]initWithFrame:CGRectMake(10, 100, self.view.frame.size.width-20, 150)];
+ [self.view addSubview:self.live];
+ 
+ - (void)startHeartDelegateRatePoint:(NSDictionary *)point {
+    NSNumber *n = [[point allValues] firstObject];
+    //拿到的数据传给心电图View
+    [self.live drawRateWithPoint:n];
+ }
+ */
 
 #import <UIKit/UIKit.h>
 
